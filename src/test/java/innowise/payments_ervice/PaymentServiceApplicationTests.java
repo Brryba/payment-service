0d349d728fbfdc62c00e3a9b21e033e26fdb302d
Yury Brazgunou <jurabrezgunov@gmail.com>
2025-08-21 11:10:19 +0300

    init

---
+++ src/test/java/innowise/payments_ervice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package innowise.payments_ervice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
